27f7425d5d2b93c15cf299655c5f8ca305757a36
Spencer Edwards <Spencer.Edwards@SJSU.EDU>
2020-06-02 15:01:27 -0700

    initial upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,149 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.css'
+
+class Square extends React.Component{
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            id: '',
+            hover: false
+        }
+        this.onMouseEnterSquare = this.onMouseEnterSquare.bind(this)
+        this.onMouseLeaveSquare = this.onMouseLeaveSquare.bind(this)
+    }
+    onMouseEnterSquare() {
+        this.setState({
+            id: 'isBackgroundRed',
+            hover: true
+        })
+    }
+    onMouseLeaveSquare() {
+        this.setState({
+            id: '',
+            hover: false})
+    }
+    render() {
+        let idState = ''
+        if(this.state.hover)
+            idState = this.state.id
+        else
+            idState = this.props.id
+        return (
+            <Button
+                className="square"
+                id = {idState}
+                onMouseEnter = {this.onMouseEnterSquare.bind()}
+                onMouseLeave = {this.onMouseLeaveSquare.bind()}
+                onClick = {this.props.onClick}>
+            </Button>)
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(100).fill(undefined),
+            exitFinished: true,
+            startMarkerIndex : -1,
+            endMarkerIndex: -1
+        }
+        this.setMarker = this.setMarker.bind(this)
+    }
+    renderSquare(count) {
+        let state = ''
+        if(count === this.state.startMarkerIndex)
+        {
+            state = 'startMarker'
+        }
+        else if(count === this.state.endMarkerIndex)
+        {
+            state = 'endMarker'
+        }
+        return (<Square id = {state} onClick = {this.setMarker.bind(this, count)} key = {count}/>)
+    }
+    setMarker(i)
+    {
+
+        let arr = this.state.squares.slice()
+        let startStateMarker = this.state.startMarkerIndex;
+        let endStateMarker = this.state.endMarkerIndex;
+        if(startStateMarker === i)
+        {
+            startStateMarker = -1
+            arr[i] = undefined
+        }
+        else if(startStateMarker < 0 && endStateMarker === i) {
+            endStateMarker = -1
+            arr[i] = undefined
+        }
+        else if(startStateMarker < 0)
+        {
+            startStateMarker = i
+            arr[i] = 'O'
+        }
+        else if(endStateMarker === i) {
+            endStateMarker = -1
+            arr[i] = undefined
+        }
+        else if(endStateMarker < 0) {
+            endStateMarker = i
+            arr[i] = 'X'
+        }
+        this.setState({
+           startMarkerIndex: startStateMarker,
+            endMarkerIndex: endStateMarker,
+            squares: arr
+        })
+        console.log("this is i: ", i)
+        console.log()
+        console.log("startMarkerIndex: " + startStateMarker)
+        console.log("endMarkerIndex: " + endStateMarker)
+    }
+    render() {
+        let parent = []
+        let count = 0
+        for(let i =0; i < 10; i++)
+        {
+            let children = []
+            for(let j = 0; j < 10; j++)
+            {
+                children.push(this.renderSquare(count))
+                count++
+            }
+            parent.push(<div key = {i}  className={"board-row"}>{children}</div>)
+        }
+
+        return (
+            <div>
+                {parent}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                </div>
+            </div>
+        );
+    }
+}
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <Game />,
+    document.getElementById('root')
+);
