2bde985490120ffa57af081179a1cb949b457eea
Spencer Edwards <Spencer.Edwards@SJSU.EDU>
2020-06-03 09:36:29 -0700

    added data structure for nodes and edges as well as a testing visualizer

--- src/DepthFirstSearch.js
+++ src/DepthFirstSearch.js
@@ -6,3 +6,64 @@
 //         x - x - x
 // # of (rows * (colums - 1)) + (rows-1 * columns)
 
+//first create graph
+
+export default class DepthFirstSearch
+{
+    createContainer(size, width)
+    {//diagonals are also valid
+        let dict = {}
+        for(let i = 0; i < size; i++)
+        {
+            if(i< width) {
+                if (i < width && i % width === 0) { //top left
+                    dict[i] = [i + 1, i + width, i + width + 1]
+                } else if (i < width && width % i === 1) //top right
+                {
+                    dict[i] = [i - 1, i + width - 1, i + width]
+                } else //first row in the middle
+                {
+                    dict[i] = [i - 1, i + 1, i + width - 1, i + width, i + width + 1]
+                }
+            }
+            else if(i >= size-width)
+            {
+                if(i%width === 0) //bottom left
+                {
+                    dict[i] = [i-width, i-width+1, i+1]
+                }//FIX BELOW AS IT IS FUCKED
+                else if(i === size-1) //right side
+                {
+                    dict[i] = [i-width-1, i-width, i-1]
+                }
+                else //a center node in the last row
+                {
+                    dict[i] = [i - width -1, i - width, i - width +1, i-1, i + 1]
+                }
+            }
+            else //should be in the center somewhere
+            {
+                if(i%width === 0)//left side not top or bottom row
+                {
+                    dict[i] = [i-width, i-width+1, i+1, i+width, i+width+1]
+                }
+                else if(i%width === width-1) //right side not top or bottom row
+                {
+                    dict[i] = [i-width-1, i-width, i-1, i+width-1, i+width]
+                }
+                else {
+                    dict[i] = [i - width - 1, i - width, i - width + 1, i - 1, i + 1, i + width - 1, i + width, i + width + 1]
+
+                }
+            }
+        }
+        return dict
+    }
+
+}
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import DepthFirstSearch from './DepthFirstSearch.js'
 class Square extends React.Component{
     constructor(props)
     {
@@ -48,14 +48,19 @@ class Board extends React.Component {
         super(props)
         this.state = {
             squares: Array(100).fill(undefined),
+            iterSquares: Array(100).fill(undefined),
+            dictionary: undefined,
             exitFinished: true,
             startMarkerIndex : -1,
-            endMarkerIndex: -1
+            endMarkerIndex: -1,
+            index : 0
         }
         this.setMarker = this.setMarker.bind(this)
+        this.animateAdjacentNodes = this.animateAdjacentNodes.bind(this)
     }
     renderSquare(count) {
         let state = 'slateGrey'
+        let squares = this.state.iter
         if(count === this.state.startMarkerIndex)
         {
             state = 'startMarker'
@@ -63,6 +68,16 @@ class Board extends React.Component {
         else if(count === this.state.endMarkerIndex)
         {
             state = 'endMarker'
+        }
+        else if(squares !== undefined)
+        {
+            if(squares[count] === 'A') {
+                state = 'green'
+            }
+            else if(squares[count] === 'B')
+            {
+                state = 'maroon'
+            }
         }
         return (<Square id = {state} onClick = {this.setMarker.bind(this, count)} key = {count}/>)
     }
@@ -99,32 +114,79 @@ class Board extends React.Component {
             endMarkerIndex: endStateMarker,
             squares: arr
         })
+
+    }
+    animateAdjacentNodes(SIZE, WIDTH, HEIGHT)
+    {
+        const k = new DepthFirstSearch()
+        let dictionary = k.createContainer(SIZE, WIDTH, HEIGHT)
+        this.setState({dictionary: dictionary})
+        console.log(dictionary)
+    }
+    componentDidMount()
+    {
+        this.timerID = setInterval(
+            () => this.tick(),
+        1000
+        )
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+    tick(){
+        let dictionary = this.state.dictionary
+        let squares = this.state.squares.slice()
+        let index = this.state.index
+        if(dictionary !== undefined && index < Object.keys(dictionary).length){
+            for (let i = 0; i < dictionary[index].length; i++) {
+                console.log(dictionary[index][i])
+                squares[dictionary[index][i]] = 'A'
+                squares[index] = 'B'
+            }
+            this.setState({
+                index: index + 1,
+                iter: squares
+            })
+        }
+        else if(dictionary !== undefined && index === Object.keys(dictionary).length)
+        {
+            this.setState({
+                index: 0,
+                iter: squares
+            })
+            clearInterval(this.timerID)
+        }
+
     }
     render() {
         let parent = []
         let count = 0
-        for(let i =0; i < 10; i++)
+        const HEIGHT = 4
+        const WIDTH = 4
+        const SIZE = HEIGHT * WIDTH
+        for(let i =0; i < HEIGHT; i++)
         {
             let children = []
-            for(let j = 0; j < 10; j++)
+            for(let j = 0; j < WIDTH; j++)
             {
                 children.push(this.renderSquare(count))
                 count++
             }
             parent.push(<div key = {i}  className={"board-row"}>{children}</div>)
         }
-
         return (
             <div>
                 {parent}
+                <Button onClick = {this.animateAdjacentNodes.bind(this, SIZE, WIDTH, HEIGHT)} > click me!</Button>
             </div>
         );
     }
 }
 
 class Game extends React.Component {
-    render() {
 
+
+    render() {
         return (
             <div id ="body" className="game">
                 <div className="game-board">
